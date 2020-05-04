6a6986bceb7e781a023ffad21fd41c57336364b9
Sheena Fernandez <sheena.fernandez78@gmail.com>
2020-05-04 20:45:24 +0900

    Express simple REST

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// 2nd parameter as callback function
+// app.all means no matter which request
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    // will continue to look for specifications which will match dishes endpoint
+    next();
+});
+
+// get request on dishes
+// implemented after next()
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+// post new dish to server
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name 
+        + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;   // Operation not supported
+    res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all the dishes!');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dish: '
+        + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/' 
+        + req.params.dishId );
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name 
+        + ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
 
 app.use(express.static(__dirname + '/public'));
 
